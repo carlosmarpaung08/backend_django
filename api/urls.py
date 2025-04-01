47d476a069fcdd807a1e175879c5dfb8318e9311
Marpaung060822 <carlosmarpaung062@gmail.com>
2025-04-01 17:05:59 +0700

    Revert "Revert "add some file for deploying""
    This reverts commit fb7831f94bdcaa6453c07d844c93a2ba938d07a1.

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import BookSearchView, RegisterUserView, LoginUserView, RecommendBooksView
+from .views import BookSearchView, RegisterUserView, LoginUserView, RecommendBooksView, UserRecommendationsView  # Tambahkan UserRecommendationsView
 
 urlpatterns = [
     path('search/', BookSearchView.as_view(), name='book-search'),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
-    path('recommend/', RecommendBooksView.as_view(), name='recommend-books'),  # Menambahkan URL untuk rekomendasi buku
+    path('recommend/', RecommendBooksView.as_view(), name='recommend-books'),  # Rekomendasi buku umum
+    path('user/recommendations/', UserRecommendationsView.as_view(), name='user-recommendations'),  # Rekomendasi buku per pengguna
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
